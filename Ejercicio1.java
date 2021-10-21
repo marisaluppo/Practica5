35da1bc56b4c3040b6b00a848a3f2bfc44bd08a7
marisaluppo <marianna.saluppo@educa.madrid.org>
2021-10-21 18:08:27 +0200

    CambiosDos

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,6 +3,7 @@ class ejercicio1 {
             public static void main(String[] args) {
                 System.out.println("¡Hola, Marianna!");
                 System.out.println("Hola, esto es un Cambio de Cristina");
+		System.out.println("Hola, esto es un cambio de Marianna");
             }
 
 }
